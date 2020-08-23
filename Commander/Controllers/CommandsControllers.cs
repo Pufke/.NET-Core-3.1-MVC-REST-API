de094eb5dbd509e6ad371c00c6eb8796141a7ab6
Pufke <pufke1998@gmail.com>
2020-08-23 23:13:30 +0200

    Updating Repository, Created new DTO, Create 3rd API enpoint to Create Resource, return 201 code

--- Commander/Controllers/CommandsControllers.cs
+++ Commander/Controllers/CommandsControllers.cs
@@ -32,13 +32,27 @@ namespace Commander.Controllers
         }
 
         //GET api/commands/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public ActionResult <CommandReadDto> GetCommandById(int id){
             var commandItem = _repository.GetCommandById(id);
             if(commandItem != null){
                 return Ok(_mapper.Map<CommandReadDto>(commandItem));
             }
             return NotFound();
+        }
+
+        //POST api/commands
+        [HttpPost]
+        public ActionResult <CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            //It's really important to pass back location of created item Code : (201 Created) , Parameter Location in Header of HTTP Response
+            return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
         }
     }
 }
